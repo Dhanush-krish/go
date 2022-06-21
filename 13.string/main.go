c864eac90c48fc78b8419e9e1f661cd0e48fdca1
dhanush <dhanush.krish1998@gmail.com>
2022-06-21 20:51:36 +0530

    go basics

---
+++ 13.string/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	str := "Strings in GO"
+	fmt.Printf("accessing first element in str: %c\n", str[0])
+
+	// finding the length of the string
+	fmt.Println("length of the string is", len(str))
+
+	// converting string to lowercase
+	fmt.Println("converting to lower case", strings.ToLower(str))
+
+	// converting string to uppercase
+	fmt.Println("converting to lower case", strings.ToUpper(str))
+
+	// converting string to array
+	fmt.Println("converting to lower case", strings.Split(str, " "))
+
+	// converting string to array
+	fmt.Println("converting to lower case", strings.Replace(str, "GO", "went", 1))
+
+}
